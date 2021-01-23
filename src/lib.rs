88a5041a6830e229b5eb2375c5b64111357e2288
Uaurá Menezes <uaurasilva@hotmail.com>
2021-01-23 14:22:19 -0300

    Remove unnecessary variable

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub struct TicTacToe {
     board: Vec<String>,
-    winner: String,
-    mark: String,
+    result: String,
     turn: i32,
     win: bool,
 }
@@ -24,23 +23,20 @@ impl TicTacToe {
         let board = (1..10).map(|_| {
             String::new()
         }).collect();
-        let winner = String::new();
-        let mark = String::new();
+        let result = String::new();
         let win = false;
         let turn = 0;
 
-        TicTacToe {board, winner, mark, turn, win}
+        TicTacToe {board, result, turn, win}
     }
 
     pub fn play(&mut self, index: usize) -> String {
         if self.board[index] == String::from("")
-        && self.turn <= 9 && !self.win{
+        && self.turn <= 9 && !self.win {
             if self.turn % 2 == 0 {
                 self.board[index] = String::from("X");
-                self.mark = String::from("X");
             } else {
                 self.board[index] = String::from("O");
-                self.mark = String::from("O");
             }
             self.turn += 1;
         }
@@ -63,23 +59,23 @@ impl TicTacToe {
             && self.board[*b] == self.board[*c]
             && self.board[*a] != String::from("") {
                 if self.board[*a] == "X" {
-                    self.winner = String::from("Player 1 Win!");
+                    self.result = String::from("Player 1 Win!");
                 } else {
-                    self.winner = String::from("Player 2 Win!");
+                    self.result = String::from("Player 2 Win!");
                 }
                 self.win = true
             } else if self.turn == 9 {
-                self.winner = String::from("It's a Draw!");
+                self.result = String::from("It's a Draw!");
             }
         }
 
-        self.mark.clone()
+        self.board[index].clone()
     }
 
     pub fn change_text(&self) -> String {
         if self.win || self.turn == 9 {
-            self.winner.clone()
-        } else if self.turn % 2 == 0{
+            self.result.clone()
+        } else if self.turn % 2 == 0 {
             String::from("Player 1 turn!")
         } else {
             String::from("Player 2 turn!")
